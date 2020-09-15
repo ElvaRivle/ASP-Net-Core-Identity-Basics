a675e06dd812aa04a5fe918896e0a3885580bbb6
ElvaRivle <elvirvlahovljak@gmail.com>
2020-09-15 23:05:40 +0200

    user class and migrations

--- Areas/Identity/Data/ApplicationUser.cs
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,16 @@ namespace IdentityTutorialGlavni.Areas.Identity.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        //identity user vec ima dosta polja ugradjenih
+        //mi ih nasljedjujemo
+        //i samo dodajemo neke svoje
+        //u nasem slucaju to su ime i prezime
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
     }
 }
