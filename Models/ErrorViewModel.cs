edd0f13b4af93c181e50d5225972d35fed5c1579
ElvaRivle <elvirvlahovljak@gmail.com>
2020-09-15 22:14:05 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityTutorialGlavni.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
