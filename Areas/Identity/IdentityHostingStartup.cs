842c9d4eed4f54f7e11bdb4fb05c5db101d8749a
ElvaRivle <elvirvlahovljak@gmail.com>
2020-09-15 22:22:36 +0200

    Scaffolded identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IdentityTutorialGlavni.Areas.Identity.Data;
+using IdentityTutorialGlavni.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityTutorialGlavni.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityTutorialGlavni.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
