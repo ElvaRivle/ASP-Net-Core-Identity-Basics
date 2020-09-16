47fa00985c7d8dab7d0d1eb395b58ac76d360d2e
ElvaRivle <elvirvlahovljak@gmail.com>
2020-09-16 12:39:51 +0200

    protect controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IdentityTutorialGlavni.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IdentityTutorialGlavni.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
