9c82ab15d27852cbcb341ade5d444439b80d5bad
ElvaRivle <elvirvlahovljak@gmail.com>
2020-09-16 12:38:24 +0200

    only alphanumeric

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -24,7 +24,7 @@ namespace IdentityTutorialGlavni.Areas.Identity
                     options.SignIn.RequireConfirmedAccount = false;
                     options.Password.RequireDigit = false;
                     options.Password.RequireUppercase = false;
-
+                    options.Password.RequireNonAlphanumeric = false;
                 })
                     .AddEntityFrameworkStores<ApplicationDbContext>();
             });
